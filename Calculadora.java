37271202339ab95a62e4bbe7454f2436f1880da0
unknown <david.perez.pgrm@gmail.com>
2024-12-09 14:26:35 +0100

    Version2

--- Calculadora.java
+++ Calculadora.java
@@ -5,6 +5,8 @@ public class Calculadora {
         suma(3,2);
         resta(10,5);
         multiplicacion(5,5);
+        division(10,2);
+        potencia2(3);
     }
     public static void suma(Integer num, Integer num1){
         System.out.println(num+num1);
@@ -14,5 +16,11 @@ public class Calculadora {
     }
     public static void multiplicacion(Integer num, Integer num1){
         System.out.println(num*num1);
+    }
+    public static void division(Integer num, Integer num1){
+        System.out.println(num/num1);
+    }
+    public static void potencia2(Integer num){
+        System.out.println(num*num);
     }
 }
