1f24b5131cd28c1ee87f07ba84f1981b2a769319
César Enrique Corral Menéndez <130445370+ccorralm01@users.noreply.github.com>
2024-12-12 12:18:16 +0100

    Añadido log y cos a Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -8,6 +8,8 @@ public class Calculadora {
         division(10,2);
         potencia2(3);
         raizCuadrada(4);//Ejemplo de Raiz
+        log(100);
+        coseno(90);
     }
     public static void suma(Integer num, Integer num1){
         System.out.println(num+num1);
@@ -31,4 +33,19 @@ public class Calculadora {
         }
         System.out.println(Math.sqrt(numero));
    }
+
+    // Parte César
+    public static void log(Integer num){
+        String resultado = num > 0
+                ? String.format(Locale.US, "%.2f", Math.log10(num))
+                : "Error: log(x) solo es válido para x > 0";
+
+        System.out.println(resultado);
+    }
+
+    public static void coseno(Integer num) {
+        double radianes = Math.toRadians(num); // Convierte grados a radianes
+        String resultado = String.format(Locale.US, "%.2f", Math.cos(radianes));
+        System.out.println(resultado);
+    }
 }
